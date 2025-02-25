a4c9105dbfdc481fc90f7a81cbc14d0300c474b1
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-02-25 11:40:12 -0300

    tela de cadastro

--- Cyber_Security_Lock/assets/js/script.js
+++ Cyber_Security_Lock/assets/js/script.js
@@ -1,15 +1,33 @@
-document.getElementById("toggleSenha").addEventListener("click", function () {
-    let inputSenha = document.getElementById("senha");
-    let icone = document.getElementById("toggleSenha");
+function toggleSenha(idInput, idIcone) {
+    let inputSenha = document.getElementById(idInput);
+    let icone = document.getElementById(idIcone);
 
-    // Alterna entre "password" e "text"
     if (inputSenha.type === "password") {
         inputSenha.type = "text";
-        icone.classList.remove("bi-eye-slash"); // Remove ícone de olho fechado
-        icone.classList.add("bi-eye"); // Adiciona ícone de olho aberto
+        icone.classList.remove("bi-eye-slash");
+        icone.classList.add("bi-eye");
     } else {
         inputSenha.type = "password";
-        icone.classList.remove("bi-eye"); // Remove ícone de olho aberto
-        icone.classList.add("bi-eye-slash"); // Adiciona ícone de olho fechado
+        icone.classList.remove("bi-eye");
+        icone.classList.add("bi-eye-slash");
     }
+}
+
+// Adiciona eventos para os dois campos de senha
+document.getElementById("toggleSenha1").addEventListener("click", function () {
+    toggleSenha("senha1", "toggleSenha1");
+});
+
+document.getElementById("toggleSenha2").addEventListener("click", function () {
+    toggleSenha("senha2", "toggleSenha2");
+});
+
+
+
+document.getElementById('cpf').addEventListener('input', function (e) {
+    let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não for número
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    e.target.value = value;
 });
