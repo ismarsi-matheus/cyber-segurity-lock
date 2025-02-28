a42d586f8dda5d5d63b2d462f9197a331d3aa47f
AntonioGodoyCarneiro <antoniogodoycarneiro@gmail.com>
2025-02-28 08:19:00 -0300

    Add files via upload

---
+++ db_cyber_security_lock (2).sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28/02/2025 às 12:15
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_cyber_security_lock`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_senha`
+--
+
+CREATE TABLE `tb_senha` (
+  `id` int(11) DEFAULT NULL,
+  `dominio` varchar(50) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `CPF` varchar(14) NOT NULL,
+  `usuario` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `nome`, `email`, `CPF`, `usuario`) VALUES
+(3, 'Cavanha', 'cavanha@gmail.com', '476.866.108-45', 'XcavanhaX'),
+(4, 'Leandro', 'leandro@gmail.com', '   .   .   -', 'dfgsdg'),
+(5, 'Leandro', 'leandro@gmail.com', '476.866.108-45', 'dfgsdg');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
