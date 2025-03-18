6acf996186b255880044c6535556318fa71719c1
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-03-18 01:55:32 -0300

    Update adicionar_senha.php

--- Cyber_Security_Lock/adicionar_senha.php
+++ Cyber_Security_Lock/adicionar_senha.php
@@ -6,38 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Senha</title>
     <link rel="stylesheet" href="assets/css/adicionar_senha.css">
-</head>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head> 
 
 <body>
     <?php
     $dsn = 'mysql:dbname=cyber_segurity_lock;host=127.0.0.1';
     $user = 'root';
     $password = '';
-
+    
     $banco = new PDO($dsn, $user, $password);
+    
     ?>
-
     <div class="container">
-        <div class="menu-icon">
-            <i class="fas fa-bars"></i>
-        </div>
-
-        <div class="form-container">
-            <h1>Adicionar Senha</h1>
-            <form action="adicionar-senha.php" method="POST">
-                <div class="input-group">
-                    <label for="name">Nome</label>
-                    <input type="text" id="name" placeholder="Digite aqui" name="dominio">
-                </div>
-                <div class="input-group">
-                    <label for="password">Senha</label>
-                    <input type="password" id="password" placeholder="Digite aqui" name="senha">
-                </div>
-                <button type="submit" class="btn btn-primary">SALVAR</button>
-                <a href="tela_inicial.php"><button type="button" class="btn btn-secondary">Voltar</button></a>
-            </form>
-        </div>
-
+        <h1><i class="fas fa-lock"></i> Adicionar Senha</h1>
+        <form action="adicionar-senha.php" method="POST">
+            <div class="input-group">
+                <label for="dominio">Domínio</label>
+                <input type="text" id="dominio" placeholder="Ex: www.exemplo.com" name="dominio">
+            </div>
+            <div class="input-group">
+                <label for="usuario">Usuário</label>
+                <input type="text" id="usuario" placeholder="Digite seu usuário" name="usuario">
+            </div>
+            <div class="input-group">
+                <label for="senha">Senha</label>
+                <input type="password" id="senha" placeholder="Digite sua senha" name="senha">
+            </div>
+            <div class="input-group">
+                <label for="nota">Nota</label>
+                <textarea id="nota" placeholder="Adicione uma observação" name="nota"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+            <a href="tela_inicial.php" ><button class="btn btn-primary btn_voltar">Voltar</button></a>
+        </form>
+    </div>
 </body>
 
 </html>
